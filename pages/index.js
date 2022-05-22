3cd7b0ce91146981a0eadab5f5fd8c4e5cafd8f0
skylerstyron <skylerstyron@gmail.com>
2022-05-21 22:14:48 -0500

    inital commit

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>deadpanprogram</title>
+        <meta name="description" content="deadpanprogram" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💾</text></svg>"></link>
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title} data-text="deadpanprogram">
+        deadpanprogram
+        </h1>
+      </main>
+
+    </div>
+  )
+}
